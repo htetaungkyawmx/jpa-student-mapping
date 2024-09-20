97e6a0970ac1893226ccd396981bd5985445f582
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-09-20 20:07:33 +0630

    Jpa Student Mapping

---
+++ src/main/java/com/example/jpastudentmapping/JpaStudentMappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpastudentmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStudentMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaStudentMappingApplication.class, args);
+    }
+
+}
