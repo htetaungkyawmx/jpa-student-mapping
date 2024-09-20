1f14b313ce2fedbbf35e2c12f2c498de3b3cebcf
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-09-20 20:22:49 +0630

    Add Subject class

--- src/main/java/com/example/jpastudentmapping/entity/Subject.java
+++ src/main/java/com/example/jpastudentmapping/entity/Subject.java
@@ -1,4 +1,24 @@
 package com.example.jpastudentmapping.entity;
 
-public class Subject extends {
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Subject extends IdClass{
+    private String subjectName;
+    private String subjectCode;
+    private double fees;
+    private int durationMonth;
+
+    public Subject(String subjectName, String subjectCode, double fees, int durationMonth) {
+        this.subjectName = subjectName;
+        this.subjectCode = subjectCode;
+        this.fees = fees;
+        this.durationMonth = durationMonth;
+    }
 }
