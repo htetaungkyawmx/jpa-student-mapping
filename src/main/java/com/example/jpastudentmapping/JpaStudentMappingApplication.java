dc8eb7835f2b170c4b62eb25ac1312f41e5e8ee3
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-09-21 09:07:01 +0630

    Add Code main

--- src/main/java/com/example/jpastudentmapping/JpaStudentMappingApplication.java
+++ src/main/java/com/example/jpastudentmapping/JpaStudentMappingApplication.java
@@ -1,13 +1,25 @@
 package com.example.jpastudentmapping;
 
+import com.example.jpastudentmapping.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaStudentMappingApplication {
+@RequiredArgsConstructor
+public class JpaStudentMappingApplication implements CommandLineRunner {
+
+    private final StudentService studentService;
 
     public static void main(String[] args) {
         SpringApplication.run(JpaStudentMappingApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        studentService.createDb();
+
     }
 
 }
